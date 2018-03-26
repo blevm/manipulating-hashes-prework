f0d1f038744c9baa487bc28f5b564998c6eb3d5f
Michelle Blevins <mjblevins7@gmail.com>
2018-03-26 20:00:03 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -8,7 +8,11 @@ def second_challenge
 
   #code your solution here!
 
-  everything = groceries.values.join(" ,")
+  everything = []
+
+  groceries.each do |item|
+    everything << item.values
+  end
 
   everything
 
